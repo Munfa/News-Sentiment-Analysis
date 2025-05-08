a56f66a74ebaa9094ed32650007ed74532b26ff8
Munfa <munfa2014@gmail.com>
2025-05-08 22:46:57 +0600

    Updated data extraction

--- news_sentiment_analysis.py
+++ news_sentiment_analysis.py
@@ -1,20 +1,33 @@
 import numpy as np
+import pandas as pd
 import requests
 import json
 
-API_key = 'Yuor API Key'
-Topic = 'ChatGPT'
-URL = f"https://gnews.io/api/v4/search?q={Topic}&lang=en&token={API_key}"
+# API_key = 'Your API key'
+# Topic = 'ChatGPT'
+# all_articles = []
+# for page in range(1,11):
+    
+#     URL = f"https://gnews.io/api/v4/search?q={Topic}&lang=en&page={page}&token={API_key}"
 
-response = requests.get(URL)
+#     response = requests.get(URL)
 
-if response.status_code==200:
-    data = response.json()
+#     if response.status_code==200:
+#         data = response.json()
+#         articles = data.get('articles', [])
+#         all_articles.append(articles)
 
-    with open('news_data.json', 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+#     else:
+#         print("No news found, error in the API response")
 
-
-else:
-    print("No news found, error in the API response")
+# with open('news_data.json', 'w') as json_file:
+#         json.dump(all_articles, json_file, indent=4)
+try:
+    with open("news_data.json", 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+except UnicodeDecodeError:
+    with open("news_data.json", 'r', encoding='latin-1') as json_file:
+        data = json.load(json_file)
+        
+print(data[:10])
 
