dec720cc700ba7a54960200f330a01c2d3f50f56
Munfa <munfa2014@gmail.com>
2025-05-13 23:19:18 +0600

    Convert data into Pandas dataframe

--- news_sentiment_analysis.py
+++ news_sentiment_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 import json
+from textblob import TextBlob
 
 # API_key = 'Your API key'
 # Topic = 'ChatGPT'
@@ -22,12 +23,30 @@ import json
 
 # with open('news_data.json', 'w') as json_file:
 #         json.dump(all_articles, json_file, indent=4)
+
 try:
     with open("news_data.json", 'r', encoding='utf-8') as json_file:
-        data = json.load(json_file)
+        nested_data = json.load(json_file)
 except UnicodeDecodeError:
     with open("news_data.json", 'r', encoding='latin-1') as json_file:
-        data = json.load(json_file)
-        
-print(data[:10])
+        nested_data = json.load(json_file)
+
+data = [article for sublist in nested_data for article in sublist]
+
+for article in data:
+    content = f"{article.get('title', '')}.{article.get('description','')}"
+    blob = TextBlob(content)
+    polarity = blob.sentiment.polarity
+    sentiment = (
+        "Positive" if polarity > 0 else
+        "Negative" if polarity < 0 else
+        "Neutral"
+    )
+    article['sentiment_score'] = round(polarity, 3)
+    article['sentiment_label'] = sentiment
+
+df = pd.DataFrame(data)
+df= df[['title', 'description', 'content', 'sentiment_score', 'sentiment_label']]
+
+print(df.head())
 
