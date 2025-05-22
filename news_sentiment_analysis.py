632947257d5b556b5c7d92fae30dcbded64c1fbf
Munfa <munfa2014@gmail.com>
2025-05-22 23:24:14 +0600

    Changed News Article Source

--- news_sentiment_analysis.py
+++ news_sentiment_analysis.py
@@ -1,40 +1,25 @@
 import numpy as np
 import pandas as pd
-import requests
 import json
 from textblob import TextBlob
+from newsapi import NewsApiClient
 
-# API_key = 'Your API key'
-# Topic = 'ChatGPT'
-# all_articles = []
-# for page in range(1,11):
-    
-#     URL = f"https://gnews.io/api/v4/search?q={Topic}&lang=en&page={page}&token={API_key}"
+# API_key = '4ca3990cb246432dab80ba9b4f234d39'
 
-#     response = requests.get(URL)
+# newsapi = NewsApiClient(API_key)
+# all_articles = newsapi.get_everything(q='Large Language Models', language='en', sort_by='relevancy')
 
-#     if response.status_code==200:
-#         data = response.json()
-#         articles = data.get('articles', [])
-#         all_articles.append(articles)
-
-#     else:
-#         print("No news found, error in the API response")
-
-# with open('news_data.json', 'w') as json_file:
+# with open('newsapi_data.json', 'w') as json_file:
 #         json.dump(all_articles, json_file, indent=4)
 
-try:
-    with open("news_data.json", 'r', encoding='utf-8') as json_file:
-        nested_data = json.load(json_file)
-except UnicodeDecodeError:
-    with open("news_data.json", 'r', encoding='latin-1') as json_file:
-        nested_data = json.load(json_file)
+with open("newsapi_data.json", 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
 
-data = [article for sublist in nested_data for article in sublist]
-
-for article in data:
-    content = f"{article.get('title', '')}.{article.get('description','')}"
+if "articles" in data:
+    articles = data["articles"]
+sentiments = []
+for article in articles:
+    content = f"{article.get('title', '')}.{article.get('description','')}.{article.get('content')}"
     blob = TextBlob(content)
     polarity = blob.sentiment.polarity
     sentiment = (
@@ -42,11 +27,6 @@ for article in data:
         "Negative" if polarity < 0 else
         "Neutral"
     )
-    article['sentiment_score'] = round(polarity, 3)
-    article['sentiment_label'] = sentiment
+    sentiments.append(sentiment)
 
-df = pd.DataFrame(data)
-df= df[['title', 'description', 'content', 'sentiment_score', 'sentiment_label']]
-
-print(df.head())
-
+print(np.unique(sentiments, return_counts=True))
