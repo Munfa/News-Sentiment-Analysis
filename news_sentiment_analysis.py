5ed1d777860d4745c8d4df6587e22e42d662ee86
Munfa <munfa2014@gmail.com>
2025-05-27 23:27:53 +0600

    Add models for analysis

--- news_sentiment_analysis.py
+++ news_sentiment_analysis.py
@@ -1,10 +1,14 @@
+import nltk.sentiment
+import nltk.sentiment.vader
 import numpy as np
 import pandas as pd
 import json
+import nltk
+from tqdm import tqdm
 from textblob import TextBlob
 from newsapi import NewsApiClient
 
-# API_key = '4ca3990cb246432dab80ba9b4f234d39'
+# API_key = 'YourAPIKey'
 
 # newsapi = NewsApiClient(API_key)
 # all_articles = newsapi.get_everything(q='Large Language Models', language='en', sort_by='relevancy')
@@ -12,21 +16,34 @@ from newsapi import NewsApiClient
 # with open('newsapi_data.json', 'w') as json_file:
 #         json.dump(all_articles, json_file, indent=4)
 
-with open("newsapi_data.json", 'r', encoding='utf-8') as json_file:
-    data = json.load(json_file)
-
-if "articles" in data:
-    articles = data["articles"]
-sentiments = []
-for article in articles:
-    content = f"{article.get('title', '')}.{article.get('description','')}.{article.get('content')}"
-    blob = TextBlob(content)
+def apply_textblob(text):
+    blob = TextBlob(text)
     polarity = blob.sentiment.polarity
     sentiment = (
         "Positive" if polarity > 0 else
         "Negative" if polarity < 0 else
         "Neutral"
     )
-    sentiments.append(sentiment)
+    return sentiment, round(polarity, 3)
 
-print(np.unique(sentiments, return_counts=True))
+def apply_vader(text):
+    vader = nltk.sentiment.vader.SentimentIntensityAnalyzer()
+    polarity = vader.polarity_scores(text)['compound']
+    sentiment = (
+        "Positive" if polarity > 0.05 else
+        "Negative" if polarity< -0.05 else
+        "Neutral"
+    )
+    return sentiment, round(polarity, 3)
+
+with open("newsapi_data.json", 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+if "articles" in data:
+    articles = data["articles"]
+
+for article in tqdm(articles, desc="Analyzing articles"):
+    content = f"{article.get('title', '')}.{article.get('description','')}"
+    blob_label, blob_score = apply_textblob(content)
+    vader_label, vader_score = apply_vader(content)
+    
