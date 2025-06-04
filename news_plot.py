7f9cb59590567e0c1661f177d7533fca31f52fd4
Munfa <munfa2014@gmail.com>
2025-06-04 12:26:15 +0600

    Added comments and updated plot

--- news_plot.py
+++ news_plot.py
@@ -17,7 +17,7 @@ ax = ax.flatten() # this turns the nested array into a single array. it helps to
 
 ###### Plotting the model sentiments #######
 for i, model in enumerate(models):
-    sns.countplot(x=df[model], palette='rainbow', ax=ax[i])
+    sns.countplot(x=df[model], palette='rainbow', ax=ax[i]) # like a bar chart it shows the occurences of classes
 
     ax[i].set_title(f'Sentiment Distribution of {titles[i]}')
     ax[i].set_xlabel('Sentiment')
@@ -26,14 +26,33 @@ for i, model in enumerate(models):
 
 ######### Plotting average model scores per week ######## 
 # Resampling to weekly averages
-df['date'] = pd.to_datetime(df['date'])
+df['date'] = pd.to_datetime(df['date']) # turning into datetime object to set it to index
 df.set_index('date', inplace=True)
-score_df = df.resample('W')[['blob_score', 'vader_score', 'distilbert_score']].mean()
-score_df = score_df.reset_index()
+score_df = df.resample('W')[['blob_score', 'vader_score', 'distilbert_score']].mean() # resampling to get the average weekly data
+score_df = score_df.reset_index() # resetting index for melting
+
+'''
+    using melt we turn the blob_score, vader_score, distilbert_score columns into rows that is in the 'Model' column
+    each model has it's score on the column next to it
+
+    such as,
+    Model       Score
+    blob_score  0.117
+    vader_score 0.997 .... and so on
+
+    easier to plot the models 
+'''
 score_df_melted = score_df.melt(id_vars='date',
                                value_vars=['blob_score', 'vader_score', 'distilbert_score'],
                                var_name='Model',
                                value_name='Score')
+
+# renaming the models to get a cleaner visual
+score_df_melted = score_df_melted.replace({
+    'blob_score': 'TextBlob',
+    'vader_score': 'VADER',
+    'distilbert_score': 'DistilBERT'
+})
 
 plt.figure(figsize=(7,5))
 sns.lineplot(data=score_df_melted, x='date', y='Score', hue='Model', palette='Set2')
--- news_sentiment_analysis.py
+++ news_sentiment_analysis.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import json
 import datetime
@@ -8,6 +7,10 @@ from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from transformers import pipeline
 
+'''
+    Using api key to get the news articles on a specific topic from NewsAPI
+    saving the json data in a json file for easier access
+'''
 # API_key = 'YourAPIKey'
 
 # newsapi = NewsApiClient(API_key)
@@ -16,6 +19,10 @@ from transformers import pipeline
 # with open('newsapi_data.json', 'w') as json_file:
 #         json.dump(all_articles, json_file, indent=4)
 
+
+
+# TextBlob is a Python library that returns Polarity scores
+# Based on this score the Positive, Negative, and Neutral labels are determined
 def apply_textblob(text):
     blob = TextBlob(text)
     polarity = blob.sentiment.polarity
@@ -26,6 +33,8 @@ def apply_textblob(text):
     )
     return sentiment, round(polarity, 3)
 
+# VADER is a rule-based sentiment analysis tool that returns Positive, Negative, Neutral and Compound scores
+# Higher Compound value means the text is Positive and lower means Negative otherwise it's Neutral
 def apply_vader(text):
     vader = SentimentIntensityAnalyzer()
     polarity = vader.polarity_scores(text)['compound']
@@ -36,30 +45,42 @@ def apply_vader(text):
     )
     return sentiment, round(polarity, 3)
 
+# using a pre-trained sentiment analysis model from Hugging Face that returns only Positive and Negative sentiments
 def apply_distilbert(text):
     distilbert = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", framework="pt")
     results = distilbert(text)
 
     return results 
 
+# using a pre-trained classification model from Hugging Face that returns multi-label sentiments from texts
 def apply_roberta(text):
     roberta = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", 
                     framework="pt")
     results = roberta(text)
     return results 
 
-
+# using encoding= utf-8 to get all characters including non-english (if exist)
 with open("newsapi_data.json", 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
+# storing the 'articles' list from the json file
 if "articles" in data:
     articles = data["articles"]
 
 contents = []
 for article in articles:
+    '''
+        looping through every article and making the title and description into one content to apply the models
+        only using raw texts to see how each model performs on the same content
+        TextBlob and VADER can process one content at a time but DistilBERT and RoBERTa makes the program slow
+        for this reason we created the contents list for those two models
+        using contents in a batch to train the models
+    '''
     content = f"{article.get('title', '')}.{article.get('description','')}"
     contents.append(content)
 
+    # the models return the label and the score used to determine the labels
+    # storing these values in new label and score for every model columns
     blob_label, blob_score = apply_textblob(content)
     article['blob_label'] = blob_label
     article['blob_score'] = blob_score
@@ -68,11 +89,14 @@ for article in articles:
     article['vader_label'] = vader_label
     article['vader_score'] = vader_score
     
+    # Changed the date format and set it to year-month-day format
     date = article.get('publishedAt', '')
     date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     date = date.strftime('%Y-%m-%d')
     article['date'] = date
 
+# these two models return the labels and scores in a list of dictionaries
+# using contents in a batch to make the program faster
 distilbert_results = apply_distilbert(contents)
 for article, result in zip(articles, distilbert_results):
     article['distilbert_label'] = result['label']
@@ -86,10 +110,10 @@ for article, result in zip(articles, roberta_results):
 # Turning the list into a pandas dataframe
 df = pd.DataFrame(articles)
 df = df.drop(['author', 'title', 'description', 'publishedAt', 'url', 'urlToImage', 'content'], axis=1)
-df['source'] = [s.get('name', '') for s in df['source']]
+df['source'] = [s.get('name', '') for s in df['source']] # 'source' is a dictionary and I'm choosing to keep only the name
 
 # Selecting the final columns
 df = df[['date', 'source', 'blob_label', 'blob_score', 'vader_label', 'vader_score',
               'distilbert_label', 'distilbert_score', 'roberta_label', 'roberta_score']]
 
-# df.to_csv('news_data.csv', index=False)
+# df.to_csv('news_data.csv', index=False) # saved the df to csv to use it for the plots
